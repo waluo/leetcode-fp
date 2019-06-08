3e7f24fe65a752cedeae3b055c914cb7ec877c1d
bubei <bubei@ricebook.com>
2019-06-08 19:40:27 +0800

    add 140

---
+++ src/main/kotlin/org/waluo/hard/140_Word_Break_II.kt
@@ -0,0 +1,24 @@
+package org.waluo.hard
+
+import org.waluo.Memo
+
+/**
+ * waluo
+ * 2019-06-08.
+ */
+fun wordBreak(s: String, wordDict: List<String>): List<String> {
+    val wordBreakMemo = Memo<String, List<String>>()
+    wordBreakMemo.f = { str ->
+        when {
+            str.isBlank() -> listOf("")
+            else -> wordDict.fold(emptyList()) { acc, c ->
+                if (str.startsWith(c) && wordBreakMemo(str.drop(c.length)).isNotEmpty()) {
+                    acc + wordBreakMemo(str.drop(c.length)).map {
+                        if (it.isNotBlank()) "$c $it" else c
+                    }
+                } else acc
+            }
+        }
+    }
+    return wordBreakMemo(s)
+}
--- src/main/kotlin/org/waluo/model.kt
+++ src/main/kotlin/org/waluo/model.kt
@@ -12,3 +12,12 @@ class TreeNode(var `val`: Int) {
 class ListNode(var `val`: Int) {
     var next: ListNode? = null
 }
+class Memo<K, V> : (K) -> V {
+    val cache: MutableMap<K, V> = mutableMapOf()
+    lateinit var f: (K) -> V
+    override fun invoke(key: K): V {
+        val v = cache[key] ?: f(key)
+        cache.putIfAbsent(key, v)
+        return v
+    }
+}
