da198dd6e74def9e800ae7ce296d506ee27c2a9c
bubei <bubei@ricebook.com>
2019-07-29 21:27:31 +0800

    add 973

---
+++ src/main/kotlin/org/waluo/medium/973_K_Closest_Points_to_Origin.kt
@@ -0,0 +1,11 @@
+package org.waluo.medium
+
+import kotlin.math.pow
+
+class Solution {
+    fun kClosest(points: Array<IntArray>, K: Int): Array<IntArray> {
+        return points.asSequence()
+            .sortedBy { it[0].toDouble().pow(2) + it[1].toDouble().pow(2) }
+            .take(K).toList().toTypedArray()
+    }
+}
