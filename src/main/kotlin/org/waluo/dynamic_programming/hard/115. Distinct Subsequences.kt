9645beb7f956e43c430dc1dfb67ab451f339b45c
bubei <1b79349b@gmail.com>
2019-10-17 19:13:06 +0800

    add 115

--- src/main/kotlin/org/waluo/dynamic_programming/hard/115. Distinct Subsequences.kt
+++ src/main/kotlin/org/waluo/dynamic_programming/hard/115. Distinct Subsequences.kt
@@ -1,2 +1,24 @@
 package org.waluo.dynamic_programming.hard
 
+import org.junit.jupiter.api.Test
+
+class Solution115 {
+    fun numDistinct(s: String, t: String): Int {
+        val dp = Array(s.length + 1) { IntArray(t.length + 1) }
+        (0..s.length).forEach { i -> dp[i][0] = 1 }
+        (1..s.length).forEach { i ->
+            (1..t.length).forEach { j ->
+                dp[i][j] = dp[i - 1][j]
+                if (s[i - 1] == t[j - 1]) {
+                    dp[i][j] += dp[i - 1][j - 1]
+                }
+            }
+        }
+        return dp[s.length][t.length]
+    }
+
+    @Test
+    fun test() {
+        println(numDistinct("rabbbit", "rabbit"))
+    }
+}
